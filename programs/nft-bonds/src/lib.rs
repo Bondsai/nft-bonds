3a816a284c952bd2ececa929f830619366712695
Aleksandr Vorobev <amigodos@yandex.ru>
2022-02-22 15:17:09 +0300

    upd

--- programs/nft-bonds/src/lib.rs
+++ programs/nft-bonds/src/lib.rs
@@ -1,25 +1,29 @@
-use std::collections::HashMap;
+use std::collections::{BTreeMap, HashMap};
 use anchor_lang::prelude::*;
 use anchor_spl::token::{self, Token, TokenAccount, Transfer};
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("8LxVy5z37GBWNHwF45XBe3DT82qdbd9Zez2BCe3BrPw1");
 
 #[program]
 pub mod nft_bonds {
     use super::*;
     pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.events = Vec::new();
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
-
-
-#[derive(Debug, Clone, AnchorSerialize, AnchorDeserialize)]
-pub struct AvailableNFT {
-    pub title: String,
+pub struct Initialize<'info> {
+    #[account(init, payer = user, space = 9000)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program <'info, System>,
 }
+
+
 
 #[derive(Debug, Clone, AnchorSerialize, AnchorDeserialize)]
 pub struct EventStruct {
@@ -27,7 +31,7 @@ pub struct EventStruct {
     pub owner: Pubkey,
     pub event_duration: u64,
     pub percent: u16,
-    pub nfts: Vec<AvailableNFT>,
+    pub nfts: Vec<Pubkey>,
 
     pub token: Pubkey,
     pub tokens_num: u64,
@@ -36,5 +40,5 @@ pub struct EventStruct {
 
 #[account]
 pub struct BaseAccount {
-    pub events: HashMap<Pubkey, Vec<EventStruct>>
+    pub events: Vec<EventStruct>
 }
--- tests/nft-bonds.js
+++ tests/nft-bonds.js
@@ -1,13 +1,42 @@
-const anchor = require("@project-serum/anchor");
+const anchor = require('@project-serum/anchor');
+const { SystemProgram } = anchor.web3;
 
-describe("nft-bonds", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.Provider.env());
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const program = anchor.workspace.NftBonds;
-    const tx = await program.rpc.initialize();
-    console.log("Your transaction signature", tx);
+const main = async() => {
+  console.log("🚀 Starting test...");
+
+  const provider = anchor.Provider.env();
+  anchor.setProvider(provider);
+
+  const program = anchor.workspace.NftBonds;
+  const baseAccount = anchor.web3.Keypair.generate();
+
+  const tx = await program.rpc.initialize({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+      systemProgram: SystemProgram.programId,
+    },
+    signers: [baseAccount],
   });
-});
+
+  console.log("📝 Your transaction signature", tx);
+
+  // Fetch data from the account.
+  let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+
+  console.log(account)
+
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+runMain();
