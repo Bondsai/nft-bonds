ea4de675e664a1fc127706e59a72bf6458b81342
Aleksandr Vorobev <amigodos@yandex.ru>
2022-02-22 11:22:25 +0300

    init

--- programs/nft-bonds/src/lib.rs
+++ programs/nft-bonds/src/lib.rs
@@ -1,4 +1,6 @@
+use std::collections::HashMap;
 use anchor_lang::prelude::*;
+use anchor_spl::token::{self, Token, TokenAccount, Transfer};
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
@@ -12,3 +14,18 @@ pub mod nft_bonds {
 
 #[derive(Accounts)]
 pub struct Initialize {}
+
+#[derive(Debug, Clone, AnchorSerialize, AnchorDeserialize)]
+pub struct EventStruct {
+    pub title: String,
+    pub owner: Pubkey,
+    pub percent: u16,
+    pub token: String,
+    pub event_duration: u64,
+    pub vesting_duration: u64
+}
+
+#[account]
+pub struct BaseAccount {
+    pub events: HashMap<Pubkey, Vec<EventStruct>>
+}
